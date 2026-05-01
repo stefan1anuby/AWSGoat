292a14934bee05b3af5180157d86b04dccdb7d83
Popa Stefan-Eduard <38299145+stefan1anuby@users.noreply.github.com>
2026-05-01 12:26:48 +0300

    Implement wait for database readiness in startup script
    Add a wait loop for the database to be ready before executing the SQL dump.

--- modules/module-2/src/script/startup.sh
+++ modules/module-2/src/script/startup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-mysql -h $RDS_ENDPOINT -P 3306 -u root -pT2kVB3zgeN3YbrKS -e "source /var/www/html/dump.sql" > /dev/null 2>&1
+until mysql -h "$RDS_ENDPOINT" -P 3306 -u root -pT2kVB3zgeN3YbrKS --ssl=0 -e "SELECT 1" > /dev/null 2>&1; do
+  echo "Waiting for database to be ready..."
+  sleep 2
+done
+
+mysql -h "$RDS_ENDPOINT" -P 3306 -u root -pT2kVB3zgeN3YbrKS --ssl=0 -e "source /var/www/html/dump.sql"
 sed -i "s,RDS_ENDPOINT_VALUE,$RDS_ENDPOINT,g" /var/www/html/config.inc
 exec apache2-foreground
